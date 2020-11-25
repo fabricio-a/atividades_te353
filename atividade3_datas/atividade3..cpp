d59a9aa86acb39f149dd7e91ded7a05144c2a79a
fabricio-a <fabricio.santana402@outlook.com>
2020-11-25 09:51:34 -0300

    atividade 3 95% concluida

--- atividade3_datas/atividade3..cpp
+++ atividade3_datas/atividade3..cpp
@@ -2,12 +2,37 @@
 //TE352 - Semana 3
 
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
 class DateOfBirth {
 	private:
 		int date[3];
+		char months[12][10] = { "Janeiro",
+			"Fevereiro",
+			"Marco",
+			"Abril",
+			"Maio",
+			"Junho",
+			"Julho",
+			"Agosto",
+			"Setembro",
+			"Outubro",
+			"Novembro",
+			"Dezembro" };
+		char zodiac[12][12] = { "Aquario",
+			"Peixes",
+			"Aries",
+			"Touro",
+			"Gemeos",
+			"Cancer",
+			"Leao",
+			"Virgem",
+			"Libra",
+			"Escorpiao",
+			"Sagitario",
+			"Capricornio" };
 		
 	public:
 		void setDate(char name[], int i) {
@@ -19,11 +44,23 @@ class DateOfBirth {
 			if(!(date[1] <= 12 && date[1] > 0)) return false;
 			
 			if(date[1]==2) {
-				if(isLeap() && date[1] =! 29) return false;
+				if(isLeap() && (date[1] =! 29)) return false;
 				else if(date[1] =! 28) return false;
+				return true;
 			}
 			
-
+			if(date[1] % 2 == 0 && date[1] > 7) {
+				if(!(date[0] == 31)) return false;
+			} 
+			else if(date[1] % 2 == 0 && date[1] < 8) {
+				if(!(date[0] == 30)) return false;
+			} 
+			else if(date[1] < 8){
+				if(!(date[0] == 30)) return false;
+			}
+			else {
+				if(!(date[0] == 31)) return false;
+			}
 			
 			return true;
 		}
@@ -38,8 +75,7 @@ class DateOfBirth {
 		}
 		
 		void printInFull() {
-			
-			cout << date[0] << " de" << " mes" << " de " << date[2];
+			cout << date[0] << " de " << months[date[1] - 1] << " de " << date[2];
 		}
 		
 		void printZodiac() {
